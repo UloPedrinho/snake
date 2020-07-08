d621bb800d454baca61d92252e8fb7d5792a84bd
pedro <pedro@montgomery.galiza>
2020-07-09 00:34:13 +0200

    snake ideas

--- src/snake.hpp
+++ src/snake.hpp
@@ -2,20 +2,21 @@
 #define SNAKE_HPP
 
 #include "helpers.hpp"
-#include <vector>
+#include <deque>
 
 class Snake {
 public:
   // TODO: constructor
   void grow(int size);
   void decrease(int size);
+  // generate current snake body
   void generate(TODO);          // TODO arguments
 private:
   int size;
   int growing {0};
   Point start_pos;
   Point end_pos;
-  std::vector<Point> body;
+  std::deque<Point> body;
 };
 
 #endif /* SNAKE_HPP */
