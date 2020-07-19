f7bf7b9ccafbe399242054c39b9419e5207930e3
pedro <pedro@montgomery.galiza>
2020-07-19 04:18:49 +0200

    cells_position fixed

--- src/game.cpp
+++ src/game.cpp
@@ -4,25 +4,31 @@ Game::Game(){
   int window_w = 400;           // FIXME
   int window_h = 400;           // FIXME
 
-  sf::Vector2f view_origin(0.f, 0.f);
-  //sf::Vector2f boardView_size()
-
+  sf::Vector2f view_origin(0.01f, 0.01f);
+  sf::Vector2f boardView_size(window_w*0.75f, window_h*0.99f);
+  sf::Vector2f infoView_size(window_w/4, window_h);
   int board_width;
   int board_height;
 
-  cell_dimension = sf::Vector2f(10.f, 10.f);
+  // cell dimension alaways a square and fixed
+  cell_dimension = sf::Vector2f(20.f, 20.f);
 
-  initBoard(board, 20, 20, Cell::Empty);
+  board_width = boardView_size.x /cell_dimension.x;
+  board_height = boardView_size.y /cell_dimension.y;
+  initBoard(board, board_width, board_height, Cell::Empty);
+
+  std::cout << board_width << "," << board_height << "\n";
+
 
   window.create(sf::VideoMode(window_w,window_h), "snake", sf::Style::Resize); // FIXME: video mode and title ;;
-  window.setFramerateLimit(20); // FIXME:  https://www.sfml-dev.org/tutorials/2.5/window-window.ph
+  window.setFramerateLimit(1); // FIXME:  https://www.sfml-dev.org/tutorials/2.5/window-window.ph
 
   // views
-  boardView_dimensions = sf::FloatRect(0.f, 0.f, window_w*0.75, window_h);
-  infoView_dimensions = sf::FloatRect(0.f, 0.f, window_w/4, window_h);
+  boardView_dimensions = sf::FloatRect(view_origin, boardView_size);
+  infoView_dimensions = sf::FloatRect(view_origin,infoView_size);
   boardView.reset(boardView_dimensions);
   infoView.reset(infoView_dimensions);
-  boardView.setViewport(sf::FloatRect(0.f, 0.f, 0.75f, 1.f));
+  boardView.setViewport(sf::FloatRect(0.01f, 0.01f, 0.75f, 0.99f));
   infoView.setViewport(sf::FloatRect(0.75f, 0.f, 0.25f, 1.f));
 
   //boardView.zoom(1.1f);
@@ -30,12 +36,10 @@ Game::Game(){
   // cells
   cells_position.setPrimitiveType(sf::Points);
 
-  // cell dimension alaways a square
-  cell_dimension.x = cell_dimension.y = boardView_dimensions.width/board.width;
 
   // cells positions
-  for (int i = 0; i < board.width; ++i) {
-    for (int j = 0; j < board.height; ++j) {
+  for (int i = 0; i < board.height; ++i) {
+    for (int j = 0; j < board.width; ++j) {
       cells_position.append(sf::Vertex(sf::Vector2f(cell_dimension.x*j,cell_dimension.y*i)));
     }
   }
@@ -67,7 +71,7 @@ void Game::update(){
 
 
 void Game::render(){
-
+  grid.clear();
   for (int i = 0; i < cells_position.getVertexCount(); ++i) {
     // cell_pos = points[i].y +
     //            points[i].x *
@@ -92,7 +96,12 @@ void Game::render(){
   a.setPosition(cells_position[1].position);
   a.setFillColor(sf::Color::Red);
 
+  b.setSize(cell_dimension);
+  b.setPosition(cells_position[18].position);
+  b.setFillColor(sf::Color::Red);
+  
 
+  // std::cout << grid.size() << "\n";
   // cell.setSize(cell_dimension);
   // cell.setPosition(40,40);
 
@@ -140,5 +149,5 @@ void Game::draw(){
     window.draw(grid[i]);
   }
   window.draw(a);
-
+  window.draw(b);
 }
